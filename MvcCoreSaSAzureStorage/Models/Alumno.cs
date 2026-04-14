7b534db448de37b8235a4b3de3c3a66f13a73159
marcospedroche05 <marcospedroche62@gmail.com>
2026-04-14 10:52:48 +0200

    Add project files.

---
+++ MvcCoreSaSAzureStorage/Models/Alumno.cs
@@ -0,0 +1,38 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace MvcCoreSaSAzureStorage.Models
+{
+    public class Alumno : ITableEntity
+    {
+        private int _IdAlumno;
+        public int IdAlumno
+        {
+            get { return this._IdAlumno; }
+            set
+            {
+                this._IdAlumno = value;
+                this.RowKey = value.ToString();
+            }
+        }
+        private string _Curso;
+        public string Curso
+        {
+            get { return this._Curso; }
+            set
+            {
+                this._Curso = value;
+                this.RowKey = value.ToString();
+            }
+        }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int Nota { get; set; }
+
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ETag ETag { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
